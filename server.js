9cadf62104e3de19956375ef2fa48efb553230ea
QuyenLH <quyenlh@vnext.com.vn>
2020-04-01 01:42:11 +0700

    change file server.js

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
 
-const app = express();
+var app = express()
+app.use(serveStatic(path.join(__dirname, 'dist')))
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/VINET-CES'));
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/VINET-CES/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+var port = process.env.PORT || 8000
+app.listen(port)
+console.log('server started ' + port)
