e3fceda8265c87e56f942636a7b8eaa9a4131e8c
hongquyen196 <hongquyen196@gmail.com>
2020-03-31 16:43:18 +0700

    Rename to "VINET-CES"
    Rename to "VINET-CES"

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/CrawlerDataFE'));
+app.use(express.static(__dirname + '/dist/VINET-CES'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/CrawlerDataFE/index.html'));
+res.sendFile(path.join(__dirname+'/dist/VINET-CES/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
